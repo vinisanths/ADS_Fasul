fb9459c43d46a5a5536e3662a7c91a8dab8e8f94
vinisanths <vinipsantos0@gmail.com>
2025-03-10 08:00:48 -0300

    adiciona scripts para geração de tabuadas e controle de fluxo

---
+++ 1ºSemestre/Algoritimos e Programação/Condicionais/for.py
@@ -0,0 +1,4 @@
+for tabuada in range(1, 11):
+    for numero in range(1, 11):
+        print(f"{tabuada} x {numero} = {tabuada * numero}")
+print("Fim da tabuada")
---
+++ 1ºSemestre/Algoritimos e Programação/Condicionais/for2.py
@@ -0,0 +1,5 @@
+for x in range(1,11):
+    if x == 3:
+        continue
+    print(x)
+print("Fim do programa")
